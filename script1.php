98a7d84f945bb51e545b9be6e2e9c2f56d017b80
jamal <ouardijamal87@gmail.com>
2023-04-03 22:44:17 +0000

    first commit

---
+++ script1.php
@@ -0,0 +1,29 @@
+<?php
+
+// Entrées
+$a = $_GET['a'];
+$b = $_GET['b'];
+
+
+
+// Traitement
+$s=$a + $b;
+
+//produit une erreur si a ou b est une chaine de caractère
+
+//Sortie
+echo "la somme est ".$s;
+
+
+
+
+/* 
+Ex2:
+    Ecrire un script php qui calcule et affiche la racine carée d'un nombre
+    n lu à partir de la requette GET
+
+
+
+
+*/
+?>
---
+++ script2.php
@@ -0,0 +1,30 @@
+<?php
+
+$n = $_GET['n'];
+
+
+$resultat = sqrt($n);
+
+print "La racine carrée est ".$resultat;
+
+print "<br>";
+
+$s = sin($n);
+
+print "le sinus est ".$s;
+
+
+
+/*
+Ex3:
+    Ecrire un script PHP qui calcule le cosins et le carée d'un nombre
+    K lu de la requette GET
+
+
+Ex4:
+    Réservation Hotel avec petit dejeuner
+
+
+*/
+
+?>
